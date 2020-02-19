52da75772cf8fab6b5543484a53d63f905fb6379
Ralf Schmitt <ralf@brainbot.com>
2020-02-19 11:09:13 +0100

    Use setup.cfg for python package declaration
    Also provide a pyproject.toml file. Unfortunately we cannot get rid of
    setup.py yet, because pip does not support editable installs without a
    setup.py file.
    
    See https://pypi.org/project/setuptools-scm/ for more information, as
    well as PEP 517 and 518.
    
    This also frees us from duplicating the dependencies in
    requirements.txt, which we sometimes fail to do (e.g. in the previous
    commit).
    
    A complete development environment can now be installed with
    
        pip install '.[dev]'

--- setup.py
+++ setup.py
@@ -1,23 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name="contract-deploy-tools",
-    setup_requires=["setuptools_scm"],
-    use_scm_version=True,
-    packages=find_packages("src"),
-    package_dir={"": "src"},
-    install_requires=[
-        "py-solc",
-        "web3>=5.0.0,<5.2",
-        "eth-tester[py-evm]",
-        "eth-utils",
-        "eth-keyfile",
-        "click",
-    ],
-    entry_points="""
-    [console_scripts]
-    deploy-tools=deploy_tools.cli:main
-    [pytest11]
-    deploy-tools=deploy_tools.plugin
-    """,
-)
+setup(use_scm_version=True)
